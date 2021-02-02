ef8802549347112e720c2723ebb5af1a40e73e28
dharikamal <76258348+dharikamal@users.noreply.github.com>
2021-02-02 20:13:06 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,35 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+let num1 = 50;
+let num2 = 20;
+let addition
+let subtraction 
+{
+ if ( num1 < num2 ){
+     
+     console.log("num1 + num2 = ", (num1 + num2))
+ }
+ else if (num1 > num2 ){
+    
+     console.log("num1 - num2 = ",(num1 - num2) )
+ }
+ else {
+     console.log("num1 = num2")
+ }
+}
+
+let example = true
+let anotherexmp = false 
+console.log(5>3)
+console.log(5 ===3)
+
+let num = (x,y) =>{
+if (x % y ===0){
+    console.log("EVEN")
+}
+else{
+    console.log("ODD")
+}
+}
+num(100,2);
